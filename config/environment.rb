b91cea7746def9029e7e44ece4d27c8872037000
Mark Muskardin <markmuskardin@gmail.com>
2011-05-12 13:57:13 -0700

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Fromthought2webRedux2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fromthought2webRedux2::Application.config.secret_token = '1488b91fe2efd25b9b4f4793933ee692ba8c17d3da9e2e95336abd6be910dcec4f42921f4bb008b722112833c0757954551aa21ded29f62f21d9a79576a35025'
